b0048964073721e275054d6eeee9654825ec87e6
Roman Pogorelov <rmk140987@gmail.com>
2020-09-23 17:57:17 +0300

    3.2. Refactoring

--- js/main.js
+++ js/main.js
@@ -8,67 +8,66 @@ const MESSAGE_TEMPLATES = [
   `Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.`,
   `Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!`
 ];
-
 const NAME_TEMPLATES = [
   `Оля`,
   `Даша`,
   `Катя`,
   `Георгий`
 ];
+const PHOTO_COUNT = 25;
+const MIN_LIKE_COUNT = 15;
+const SHIFT_LIKE_COUNT = 185;
+const MAX_COMMENT_COUNT = 3;
+const AVATAR_COUNT = 6;
+const PICTURE_TEMPLATE = document.querySelector(`#picture`).content.querySelector(`a.picture`);
+const PICTURES = document.querySelector(`.pictures`);
 
-/**
- * Function generates array with js objects of the photo descriptions.
- * @return {[]} generated array.
- */
-function generateJSObjectArray() {
-  let size = 25;
-  let result = [];
-  for (let i = 1; i <= size; i++) {
-    let obj = {};
-    obj.url = `photos/${i}.jpg`;
-    obj.description = `descr - ${i}`;
-    obj.likes = Math.floor(15 + Math.random() * 185);
-    obj.comments = [];
-    for (let j = 0, likeAmount = Math.ceil(Math.random() * 3); j < likeAmount; j++) {
-      let comment = {};
-      comment.avatar = `img/avatar-${Math.ceil(Math.random() * 6)}.svg`;
-      comment.message = MESSAGE_TEMPLATES[Math.floor(Math.random() * MESSAGE_TEMPLATES.length)];
-      comment.name = NAME_TEMPLATES[Math.floor(Math.random() * NAME_TEMPLATES.length)];
-      obj.comments.push(comment);
-    }
-    result.push(obj);
+function generateComments() {
+  const result = [];
+  const commentCount = Math.ceil(Math.random() * MAX_COMMENT_COUNT);
+  for (let i = 0; i < commentCount; i++) {
+    const generatedAvatarValue = `img/avatar-${Math.ceil(Math.random() * AVATAR_COUNT)}.svg`;
+    const generatedMessageValue = MESSAGE_TEMPLATES[Math.floor(Math.random() * MESSAGE_TEMPLATES.length)];
+    const generatedNameValue = NAME_TEMPLATES[Math.floor(Math.random() * NAME_TEMPLATES.length)];
+    result.push({
+      avatar: generatedAvatarValue,
+      message: generatedMessageValue,
+      name: generatedNameValue
+    });
   }
   return result;
 }
 
-/**
- * Function creates DOM element based on js object.
- * @param {object} template is an html template for the DOM element
- * @param {object} data is the based js object
- * @return {ActiveX.IXMLDOMNode | Node} a ready node.
- */
-function createDOMElement(template, data) {
-  let result = template.cloneNode(true);
+function generatePictures(count) {
+  const result = [];
+  for (let i = 0; i < count; i++) {
+    const generatedUrlValue = `photos/${i + 1}.jpg`;
+    const generatedDescriptionValue = `descr - ${i + 1}`;
+    const generatedLikesValue = Math.floor(MIN_LIKE_COUNT + Math.random() * SHIFT_LIKE_COUNT);
+    result.push({
+      url: generatedUrlValue,
+      description: generatedDescriptionValue,
+      likes: generatedLikesValue,
+      comments: generateComments()
+    });
+  }
+  return result;
+}
+
+function createPicture(data) {
+  const result = PICTURE_TEMPLATE.cloneNode(true);
   result.querySelector(`.picture__img`).src = data.url;
   result.querySelector(`.picture__likes`).textContent = data.likes;
   result.querySelector(`.picture__comments`).textContent = data.comments.length.toString();
   return result;
 }
 
-/**
- * Function fills a fragment by the objects and it adds the fragment to the page.
- * @param {[]} dataArray is an array of the objects to fill the fragment.
- * @param {String} target is a class of the target element to add the fragment.
- */
-function createDOMElements(dataArray, target) {
-  let template = document.querySelector(`#picture`)
-    .content
-    .querySelector(`a.picture`);
-  let fragment = document.createDocumentFragment();
+function createPictures(dataArray) {
+  const fragment = document.createDocumentFragment();
   for (let data of dataArray) {
-    fragment.appendChild(createDOMElement(template, data));
+    fragment.appendChild(createPicture(data));
   }
-  document.querySelector(target).appendChild(fragment);
+  PICTURES.appendChild(fragment);
 }
 
-createDOMElements(generateJSObjectArray(), `.pictures`);
+createPictures(generatePictures(PHOTO_COUNT));
